3203e507b697cce4456f4fb424a0ec20824a0435
ngoanvt94 <tranngoan2801@gmail.com>
2024-09-17 20:10:39 +0700

    comment

---
+++ Javabai4_pheptinhcoban/src/Bai4/Bai4_Pheptinhcoban17sep.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bai4;
+
+import java.util.Scanner;
+/**
+ *
+ * @author SPC
+ */
+public class Bai4_Pheptinhcoban17sep {
+    public static void main(String[] args) {
+        System.out.println("Chuong trinh tinh hieu 2 so");
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Nhap so a: ");
+        int a = sc.nextInt();      
+         System.out.println("Nhap so b: ");
+        int b = sc.nextInt();
+        System.out.println(a + " - " + b + " = " + tinhthuong(a,b));
+    }
+    public static int tinhtong(int a, int b){
+        return a + b;
+    }
+    public static int tinhhieu(int a, int b){
+        return a - b;
+    }
+    public static int tinhtich(int a, int b){
+        return a * b;
+    }
+    public static String tinhthuong(int a, int b){
+        if(b ==0){
+            return "Khong the chia voi 0";
+        }
+        else{
+            return Double.toString(a/b);
+        }
+            
+    }
+}    
+        
+           
+        
+    
+   
+
